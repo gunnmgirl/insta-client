93cd676902c80a740762b2d8c7e4636c9b742725
gunnmgirl <gunnmgirl@gmail.com>
2020-12-23 23:59:38 +0100

    Adds cypress test for log in when user email does not exist

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe("The Log In Suite", () => {
-  it("does not work with wrong credentials", () => {
+  it("does not work with wrong password", () => {
     cy.visit("http://localhost:3000");
     cy.get("[data-cy=log-in-link]").click();
     cy.get("[data-cy=email-input]").type("naid@gmail.com");
@@ -10,7 +10,20 @@ describe("The Log In Suite", () => {
     cy.get("[data-cy=error-message]").contains("You entered a wrong password!");
     cy.location("pathname").should("equal", "/login");
   });
-  it("happy path", () => {
+
+  it("does not work with wrong email", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("[data-cy=log-in-link]").click();
+    cy.get("[data-cy=email-input]").type("nn@gmail.com");
+    cy.get("[data-cy=password-input]").type("12345678");
+    cy.get("[data-cy=log-in-button]").click();
+    cy.get("[data-cy=error-message]").contains(
+      "A user with this email could not be found!"
+    );
+    cy.location("pathname").should("equal", "/login");
+  });
+
+  it("happy workflow", () => {
     cy.visit("http://localhost:3000");
     cy.get("[data-cy=log-in-link]").click();
     cy.get("[data-cy=email-input]").type("naid@gmail.com");
